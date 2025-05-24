fafbc9361a5bfb974fc2137d0dc25fa94b5ce2f5
longtou.2024 <longtou.2024@kakaoent.com>
2025-05-24 13:07:00 +0000

    tmp; t4 asr oom

--- preprocessors/Emilia/env.sh
+++ preprocessors/Emilia/env.sh
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 conda install ffmpeg -y
-conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
+#conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
+pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu121
 pip install -r requirements.txt
 pip install onnxruntime-gpu --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/
