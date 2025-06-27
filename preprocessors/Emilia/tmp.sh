123be15ebf0faa900214d475aa528f716887c661
longtou.2024 <longtou.2024@kakaoent.com>
2025-06-27 07:17:49 +0000

    add script for whispering

--- preprocessors/Emilia/tmp.sh
+++ preprocessors/Emilia/tmp.sh
@@ -1,3 +1,3 @@
 
-python main_wds.py --config_path /home/longtou.2024/mount/longtou/saved/Emilia/config.json --input_folder_path "gs://ai-lab-speech-bucket/longtou/db/mediazen_teen/wds_v2/shard-000000.tar" --gcs_url "gs://ai-lab-speech-bucket/longtou/db/mediazen_teen/" --split_stereo
+python run_main_wds_whispering.py --config_path /home/longtou.2024/mount/longtou/saved/Emilia/config.json --input_folder_path "/home/longtou.2024/mount/longtou/db/whispering/wds_v2/shard-000000.tar" --gcs_url "gs://ai-lab-speech-bucket/longtou/db/whispering/" --wds_path emilia_pipe
 #--max_gpu_mem_frac 0.5 --start_shard 100 --f_log log0.txt
