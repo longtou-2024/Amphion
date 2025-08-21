199ec358b2037e76fbb27277627b7e9d525ae63f
longtou.2024 <longtou.2024@kakaoent.com>
2025-08-21 07:43:20 +0000

    bugfix: onnxruntime-gpu conflict with onnxruntime

--- preprocessors/Emilia/h100/run_h100_lite.py
+++ preprocessors/Emilia/h100/run_h100_lite.py
@@ -14,7 +14,7 @@ from kfp import kubernetes
 from kfp.dsl import PipelineTask
 from kfp.kubernetes import common
 
-IMAGE_URL = "us-central1-docker.pkg.dev/prod-ai-project/tts/amphion:v5.0"
+IMAGE_URL = "us-central1-docker.pkg.dev/prod-ai-project/tts/amphion:v5.1"
 N_GPU = 1
 N_CPU = "12"
 MEM_SIZE = "100Gi"
