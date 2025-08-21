50e35e45ded5c3eebdb295236f09d0529df955f3
longtou.2024 <longtou.2024@kakaoent.com>
2025-08-21 05:17:35 +0000

    add main_wds_lite.py for noise reduction

--- preprocessors/Emilia/h100/run_h100_lite.py
+++ preprocessors/Emilia/h100/run_h100_lite.py
@@ -14,24 +14,24 @@ from kfp import kubernetes
 from kfp.dsl import PipelineTask
 from kfp.kubernetes import common
 
-IMAGE_URL = "us-central1-docker.pkg.dev/prod-ai-project/tts/amphion:v4.0"
+IMAGE_URL = "us-central1-docker.pkg.dev/prod-ai-project/tts/amphion:v5.0"
 N_GPU = 1
 N_CPU = "12"
 MEM_SIZE = "100Gi"
 MOUNT_PATH = "/home/longtou.2024/mount"
 CONFIG_PATH = f"{MOUNT_PATH}/longtou/saved/Emilia/config.json"
-WDS_PATH = "emilia_pipe"
-RECIPE_NAME = "ku_old"
-INPUT_FOLDER_PATH = "gs://prod-ai-lab-speech-bucket/longtou/db/" + RECIPE_NAME + "/wds_v2/shard-00{0000..0619}.tar"
-INPUT_FOLDER_PATH2 = "gs://prod-ai-lab-speech-bucket/longtou/db/" + RECIPE_NAME + "/wds_v2/shard-00{0620..1239}.tar"
+WDS_PATH = "emilia_pipe_lite"
+RECIPE_NAME = "speechlabs"
+INPUT_FOLDER_PATH = "gs://prod-ai-lab-speech-bucket/longtou/db/" + RECIPE_NAME + "/wds_v2/shard-000{000..084}.tar"
+INPUT_FOLDER_PATH2 = "gs://prod-ai-lab-speech-bucket/longtou/db/" + RECIPE_NAME + "/wds_v2/shard-000{085..169}.tar"
 GCS_URL = "gs://prod-ai-lab-speech-bucket/longtou/db/" + RECIPE_NAME + "/"
-F_LOG = MOUNT_PATH + "/longtou/db/" + RECIPE_NAME +"/emilia_pipe_0.log"
-F_LOG2 = MOUNT_PATH + "/longtou/db/" + RECIPE_NAME +"/emilia_pipe_1.log"
+F_LOG = MOUNT_PATH + "/longtou/db/" + RECIPE_NAME +"/emilia_pipe_lite_0.log"
+F_LOG2 = MOUNT_PATH + "/longtou/db/" + RECIPE_NAME +"/emilia_pipe_lite_1.log"
 # --split_stereo
 SHELL_COMMAND = f''' \
 export CUDA_VISIBLE_DEVICES="0" \
 && . ./activate_python.sh \
-&& python main_wds.py --config_path {CONFIG_PATH} --input_folder_path {INPUT_FOLDER_PATH} --wds_path {WDS_PATH} --gcs_url {GCS_URL} --f_log {F_LOG} --max_gpu_mem_frac 0.5 --start_shard 0 > /dev/null 2>&1 & sleep 10 && export CUDA_VISIBLE_DEVICES="0" && . ./activate_python.sh && python main_wds.py --config_path {CONFIG_PATH} --input_folder_path {INPUT_FOLDER_PATH2} --wds_path {WDS_PATH} --gcs_url {GCS_URL} --f_log {F_LOG2} --max_gpu_mem_frac 0.5 --start_shard 1000
+&& python main_wds_lite.py --config_path {CONFIG_PATH} --input_folder_path {INPUT_FOLDER_PATH} --wds_path {WDS_PATH} --gcs_url {GCS_URL} --f_log {F_LOG} --max_gpu_mem_frac 0.5 --start_shard 0 > /dev/null 2>&1 & sleep 10 && export CUDA_VISIBLE_DEVICES="0" && . ./activate_python.sh && python main_wds_lite.py --config_path {CONFIG_PATH} --input_folder_path {INPUT_FOLDER_PATH2} --wds_path {WDS_PATH} --gcs_url {GCS_URL} --f_log {F_LOG2} --max_gpu_mem_frac 0.5 --start_shard 100
 '''
 
 def add_pod_annotation(
---
+++ preprocessors/Emilia/run_main_wds_lite.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#--input_folder_path "gs://prod-ai-lab-speech-bucket/longtou/db/speechlabs/wds_v2/shard-000000.tar" \
+python main_wds_lite.py \
+    --config_path "/home/longtou.2024/mount/longtou/saved/Emilia/config.json" \
+    --input_folder_path "gs://prod-ai-lab-speech-bucket/longtou/tmp/speechlabs/wds_v2/shard-000000.tar" \
+    --wds_path emilia_pipe_lite \
+    --gcs_url "gs://prod-ai-lab-speech-bucket/longtou/tmp/speechlabs/" \
+    --f_log "emilia_pipe.log"
