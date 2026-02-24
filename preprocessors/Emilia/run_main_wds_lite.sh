9396f7c7664974d27acb8a279a879ce19fcfeb63
longtou.2024 <longtou.2024@kakaoent.com>
2026-02-24 06:01:52 +0000

    minor sync

--- preprocessors/Emilia/run_main_wds_lite.sh
+++ preprocessors/Emilia/run_main_wds_lite.sh
@@ -3,7 +3,7 @@
 #--input_folder_path "gs://prod-ai-lab-speech-bucket/longtou/db/speechlabs/wds_v2/shard-000000.tar" \
 python main_wds_lite.py \
     --config_path "/home/longtou.2024/mount/longtou/saved/Emilia/config.json" \
-    --input_folder_path "gs://prod-ai-lab-speech-bucket/longtou/tmp/speechlabs/wds_v2/shard-000000.tar" \
+    --input_folder_path "gs://prod-ai-lab-speech-bucket/longtou/tmp/diquest_child/wds_v2/shard-000000.tar" \
     --wds_path emilia_pipe_lite \
-    --gcs_url "gs://prod-ai-lab-speech-bucket/longtou/tmp/speechlabs/" \
+    --gcs_url "gs://prod-ai-lab-speech-bucket/longtou/tmp/diquest_child/" \
     --f_log "emilia_pipe.log"
